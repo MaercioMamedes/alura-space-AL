afe0a477d5159ccb13467a6b8cdde910df561f14
MaercioMamedes <maerciomamedes@hotmail.com>
2022-10-15 19:42:16 -0300

    completion of the first part of the course

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from gallery.views import index
+from gallery.views import index, object_astronomic
 
-
+app_name = 'gallery'
 urlpatterns =[
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('object', object_astronomic, name='object_astronomic'),
+
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'gallery/index.html')
+
+def object_astronomic(request):
+    return render(request, 'gallery/imagem.html')
