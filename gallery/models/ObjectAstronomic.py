5e25de614f53324e0dfccb44f5d09073d44d95ca
MaercioMamedes <maerciomamedes@hotmail.com>
2023-03-19 00:31:18 -0300

    first migrate

---
+++ gallery/models/ObjectAstronomic.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class ObjectAstronomic(models.Model):
+    title = models.CharField('Título', max_length=500)
+    description = models.TextField('Descrição')
+    image = models.ImageField('Foto', upload_to='pictures/%Y/%m/%d', blank=True)
