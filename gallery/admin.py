1a8dac0e3bc252272d525e04a6a3feac0742d9cd
MaercioMamedes <maerciomamedes@hotmail.com>
2023-03-21 22:50:05 -0300

    admin gallary

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from gallery.models import ObjectAstronomic
 # Register your models here.
+
+
+@admin.register(ObjectAstronomic)
+class ObjectAtronomic(admin.ModelAdmin):
+    pass
